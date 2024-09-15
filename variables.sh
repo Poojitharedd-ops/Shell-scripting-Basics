7a1ea441fc005622ffcccbed8aacb363f3a1035c
poojitha <poojithadspr@gmail.com>
2024-09-15 11:28:36 +0530

    variables3

--- variables.sh
+++ variables.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
  echo "please enter username:"
  read username
+ echo "entered username is $useranme"
  echo "please enter password:"
+
  read password
+ echo "entered password is $password"
